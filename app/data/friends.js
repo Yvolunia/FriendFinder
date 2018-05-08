449c18969b047dacd978c6934a5fc8d9c1af611c
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2018-05-08 06:19:11 -0400

    Update Friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,68 @@
-friends.js
+
+var friends = [
+    {
+      name: "Jerry Alexander",
+      photo: "https://images.pexels.com/photos/555790/pexels-photo-555790.png?cs=srgb&dl=black-and-white-black-and-white-boy-555790.jpg&fm=jpg, https://www.pexels.com/photo/man-in-crew-neck-shirt-555790/",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Elain Richards",
+      photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?cs=srgb&dl=beautiful-brunette-cute-774909.jpg&fm=jpg."
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Ruth Stiller",
+      photo: "https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg?cs=srgb&dl=adult-beautiful-close-up-773371.jpg&fm=jpg, https://www.pexels.com/photo/woman-wearing-eyeglasses-773371/",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Michael Harris",
+      photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=adult-beard-boy-220453.jpg&fm=jpg, https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+  
+  module.exports = friends;
--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-server.js
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
